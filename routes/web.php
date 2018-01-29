8835b50102d841bb5ce7a182bd3a0e882538a865
sujit bista <sujitbistaprogrammer@gmail.com>
2018-01-29 12:14:30 +0545

    added named route to login

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', 'ProductController@getIndex')->name('product.index');
 Route::get('user/signup','UserController@getIndex');
 Route::post('user/signup','UserController@postSignUp')->name('user.signup');
 
-Route::get('user/signin','UserController@getSignIn');
+Route::get('user/signin','UserController@getSignIn')->name('login');
 Route::post('user/signin','UserController@postSignIn')->name('user.signin');
 
 Route::get('user/profile','UserController@getProfile')->name('user.profile')->middleware('auth');
